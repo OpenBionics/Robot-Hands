80866fa7f079ed3f642ff77203a3079c8d82e6de
Agis Zisimatos <azisi@windowslive.com>
2014-11-23 20:48:32 +0200

    Add new pictures of robot hand.

---
+++ Software/ROS/openbionics (Case Conflict)/scripts/Main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('openBionics')
+
+import sys
+
+import rospy
+from openBionics.srv import *
+
+def robotHand_client(req):
+	rospy.wait_for_service('robotHand_srv')
+	try:
+		robotHand_srv = rospy.ServiceProxy('robotHand_srv', hand)
+ 		resp = robotHand_srv(req)
+ 		return resp
+ 	except rospy.ServiceException, e:
+ 		print "Service call failed: %s"%e
+
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		aperture = float(sys.argv[1])
+		if (aperture < 0 or aperture > 1):
+			print "[ERROR]: Bad input [0,1]"
+			sys.exit(1)
+	else:
+		print "[ERROR]: Bad input [0,1]"
+		sys.exit(1)
+   	
+	print "%s"%robotHand_client(aperture)
